e5e120b16e84d443a121de074517c9637131d54e
lab3 <s6135512036@phuket.psu.ac.th>
2022-04-20 04:01:08 +0700

    sec

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,8 +3,8 @@ import React from 'react'
 function HeaderItem({ Icon,title}) {
   return (
       <div>
-          <Icon className="h-8"/>
-          <p>{title}</p>
+          <Icon className="h-8 mb-1"/>
+          <p className='tracking-widest'>{title}</p>
       </div>
     
 
